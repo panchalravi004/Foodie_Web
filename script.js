b8b8e3dd0d45adae42d1cb56ceecb81ee7bafdca
Ravi Panchal <panchalravi018@gmail.com>
2022-02-16 09:51:33 +0530

    Sigin In Completed

--- script.js
+++ script.js
@@ -1,21 +1,41 @@
 //For Bar
-function show_bar(){
-    var a =  document.getElementById("header");
+function show_bar() {
+    var a = document.getElementById("header");
     a.classList.toggle('active');
 }
 // for search 
-function show_search(){
-    var b =  document.getElementById("search");
+function show_search() {
+    var b = document.getElementById("search");
     b.classList.toggle('active');
+}
+//for sigin button click
+function show_sigin() {
+    var a = document.getElementById("sign-in");
+    if (a.style.display = "none") {
+        a.style.display = "flex"
+    }
+    else {
+        a.style.display = "none"
+    }
+}
+//for close signin
+function close_signin() {
+    var a = document.getElementById("sign-in");
+    if (a.style.display = "flex") {
+        a.style.display = "none"
+    }
+    else {
+        a.style.display = "flex"
+    }
 }
 //show header on the top
 $(document).ready(
-    function()
-    {
+    function () {
         $("#header").load("header/header.html");
         $("#footer").load("footer/footer.html");
         $("#offer-container").load("offers/offer.html");
         $("#product").load("products/product.html");
         $("#category").load("category/category.html");
+        $("#sign-in").load("signin/signin.html");
     }
 );
--- script/anime.js
+++ script/anime.js
@@ -12,10 +12,17 @@ $(window).scroll(function () {
             "border-bottom-right-radius": "10px"
         });
     }
+    /* Category Before css*/
+    if ($(this).scrollTop() > 50) {
+        $(".category").append('<style>.category:before{height: 60%;border-bottom-left-radius: 50%;border-bottom-right-radius: 50%;}</style>');
+    }
+    else {
+        $(".category").append('<style>.category:before{height:0%;border-bottom-left-radius:0%;border-bottom-right-radius:0%;}</style>');
+    }
     /*for circle only */
     if ($(this).scrollTop() > 20) {
         // alert($(this).scrollTop() + " px");
-        $(".product").append('<style>.product:before{clip-path: circle(' + ($(this).scrollTop() * 2)/10 + 'rem at 50% 50%)}</style>');
+        $(".product").append('<style>.product:before{clip-path: circle(' + ($(this).scrollTop() * 2) / 10 + 'rem at 50% 50%)}</style>');
         if ($(this).scrollTop() > 500) {
             $(".product").append('<style>.product:before{clip-path: circle(100rem at 50% 50%)}</style>');
         }
